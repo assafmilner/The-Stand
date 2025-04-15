544070176d4dc56ca577a7fef45c7bb1e1b23d49
Assaf Milner <assafmilner@gmail.com>
2025-04-15 18:12:59 +0300

    User Model new

--- fan-server/app.js
+++ fan-server/app.js
@@ -6,10 +6,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-
+const path = require("path");
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 // Middlewares
 app.use(cors());
 app.use(express.json()); // כדי לעבוד עם JSON
+
+
 
 // בדיקה ראשונית
 app.get("/", (req, res) => {
---
+++ fan-server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+   },
+   role: {
+    type: String,
+    default: "user",
+  },
+  favoriteTeam: {
+    type: String,
+    enum: ['הפועל תל אביב','מכבי תל אביב','הפועל באר שבע', 'מכבי חיפה', 'בית"ר ירושלים', 'בני יהודה', 'מכבי נתניה', 'הפועל חיפה', 'הפועל ירושלים', 'עירוני קרית שמונה', 'מ.ס. אשדוד', 'בני סכנין', 'הפועל פתח תקווה', 'מכבי פתח תקווה', 'הפועל רמת גן', 'הפועל כפר שלם'],
+    default: "הפועל תל אביב"
+  },
+  location: {
+    type: String,
+    enum: ['צפון', 'מרכז', 'דרום', 'ירושלים', 'אחר'],
+    default: 'אחר'
+  },
+  refreshToken: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+    default:"http://localhost:3001/assets/defaultProfilePic.png", 
+});
