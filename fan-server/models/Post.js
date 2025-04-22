dc7527c004d4e6b7b96bb81aa81f8dce8ed08f53
Assaf Milner <assafmilner@gmail.com>
2025-04-22 15:42:36 +0300

    server feed

--- fan-server/models/Post.js
+++ fan-server/models/Post.js
@@ -6,24 +6,29 @@ const PostSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+  
   communityId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Community",
     required: true,
   },
+
   content: {
     type: String,
     required: true,
   },
+
   media: {
     type: [String],
     default: [],
   },
+
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     default: [],
   }],
+
   createdAt: {
     type: Date,
     default: Date.now,
